333a46f70a7aa50710d7b5fcc38bc7770e5e7ae7
tomaspropato023 <166726520+tomaspropato023@users.noreply.github.com>
2025-06-04 19:48:11 -0300

    Ajustes en registro y script en login para validar info

---
+++ InicioSesion/login/script.js
@@ -0,0 +1,16 @@
+let loginButton = document.querySelector("#login");
+
+let emailEj = "tomas.propato@outlook.com";
+let passEj = "123123123";
+
+loginButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    if (email != emailEj || password != passEj) {
+        alert("Uno de los campos es incorrecto.");
+    } else {
+        alert("Inicio de sesión exitoso!")
+    }
+})
