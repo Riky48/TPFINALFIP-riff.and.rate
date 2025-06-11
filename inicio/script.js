aaf7cd836dc50261e9e88d9a134435be2e5f1606
Santiago Ariel Sallago <Santiagosallago09@gmail.com>
2025-06-11 20:46:57 -0300

    comienzo del responsive y cambios ligeros en el javascript

--- inicio/script.js
+++ inicio/script.js
@@ -98,7 +98,7 @@ async function publicaciones(){
         
         <div class="user">
         <img src="${usr.picture.medium}" alt="Foto de ${usr.name.first}">
-          <p><strong>${usr.name.first} ${usr.name.last}</strong> (${usr.gender})</p>
+          <p>${usr.name.first} ${usr.name.last}</p>
         </div>
           <div class="imagecontent">
           <p>${commentuser.body}</p>
