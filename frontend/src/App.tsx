a719d8debe95de1e918e511c1a73139a5d411863
Santiago Ariel Sallago <Santiagosallago09@gmail.com>
2025-09-01 22:10:48 -0300

    organizacion de las carpetas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import './App.css';
-import { Inicio } from './Pages/Inicio';
+import { Inicio } from './Pages/feed/Inicio';
 import { Nav } from './components/Nav/Nav';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Marketplace from './Pages/Marketplace';
+import Marketplace from './Pages/marketplace/Marketplace';
 import { useState } from 'react';
 
 function App() {
@@ -17,6 +17,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Inicio />} />
             <Route path="/marketplace" element={<Marketplace />} />
+            
           </Routes>
         </div>
       </div>
