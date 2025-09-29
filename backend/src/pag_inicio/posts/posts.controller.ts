0d0f03e36651887f2e5e7480fb43ff35a2121c47
Santiago Ariel Sallago <Santiagosallago09@gmail.com>
2025-09-29 20:47:33 -0300

    Cambios de los cuales no tengo ni idea de que hice.
    No me pregunten

--- backend/src/pag_inicio/posts/posts.controller.ts
+++ backend/src/pag_inicio/posts/posts.controller.ts
@@ -1,54 +1,41 @@
-import { Controller, Post, Body, Param, Delete, ParseIntPipe, UseInterceptors, UploadedFile, NotFoundException } from '@nestjs/common';
+import { Controller, Post, Body, Param, Delete, ParseIntPipe, UseInterceptors, NotFoundException, UploadedFiles, Put } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { extname } from 'path';
 import { diskStorage } from 'multer';
+import { UpdatePostDto } from './dto/update-post.dto';
 
 @Controller('posts')
 export class PostsController {
-  constructor(private readonly postsService: PostsService) {}
+  constructor(private readonly postsService: PostsService) { }
 
   @Post('upload')
-  @UseInterceptors(FileInterceptor('file',{
+  @UseInterceptors(FilesInterceptor('files', 10, {
     storage: diskStorage({
       destination: './uploads',
       filename: (req, file, cb) => {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
         const ext = extname(file.originalname);
-        cb(null,`${file.fieldname}-${uniqueSuffix}${ext}`);
+        cb(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
       }
     })
   }))
-   create(
-    @UploadedFile() file: Express.Multer.File,
+  create(
+    @UploadedFiles() files: Express.Multer.File[],
     @Body() createPostDto: CreatePostDto,
   ) {
-    if (!file) {
-      throw new NotFoundException('Archivo no subido');
-    }
-    return this.postsService.createPostWithFile(createPostDto, file);
+    
+    return this.postsService.createPostWithFiles(createPostDto, files);
   }
-
-
-
-  // @Get('all')
-  // findAll() {
-  //   return this.postsService.findAll();
-  // }
-
-  // @Get(':id')
-  // findOne(@Param('id', ParseIntPipe) id: number) {
-  //   return this.postsService.findOne(+id);
-  // }
-
-  // @Patch(':id')
-  // update(@Param('id', ParseIntPipe) id: number, @Body(new ValidationPipe({ transform: true })) updatePostDto: UpdatePostDto) {
-  //   return this.postsService.update(+id, updatePostDto);
-  // }
 
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.postsService.remove(+id);
+  }
+
+  @Put(':id')
+  update(@Param('id', ParseIntPipe) id: number, @Body() updatePostDto: UpdatePostDto) {
+    return this.postsService.update(+id, updatePostDto);
   }
 }
