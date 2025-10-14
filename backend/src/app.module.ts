df6ae810f210720c56e0da545992b26237eb2263
Riky <born.in.winter48@gmail.com>
2025-10-14 01:18:54 -0300

    servidor back andando

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,20 +1,49 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ProductoModule } from './marketplace/producto.module';
+import { PostsModule } from './pag_inicio/posts/posts.module';
+import { FeedModule } from './feed/feed.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+
+
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'riffandrate',
-      autoLoadEntities: true,
-      synchronize: true, // cuidado en prod!
+  imports: 
+  [ ConfigModule.forRoot({
+      isGlobal: true,
+  }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+      "type": 'mysql',
+      "host": config.get<string>('DB_HOST'),
+      "port": config.get<number>('DB_PORT'),
+      "username": config.get<string>('DB_USER'),
+      "password": config.get<string>('DB_PASSWORD'),
+      "database": config.get<string>('DB_NAME'),
+      "entities": [
+              "dist//.entity{.ts,.js}",
+      ],
+      "synchronize": true,
+      extra:{
+        connectionLimit: 10,
+        connectTimeout: 5000,
+      },
+      retryAttempts: 6,
+      retryDelay: 3000,
     }),
-    ProductoModule,
+    }),
+
+    PostsModule,
+    FeedModule,
+    UserModule,
+    AuthModule,
+    
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- backend/src/marketplace/producto.module.ts
+++ backend/src/marketplace/producto.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Producto } from './entities/Producto.entity';
 import { ProductoService } from './producto.service';
 import { ProductoController } from './producto.controller';
+import { _user } from 'src/feed/entities/_user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Producto])],
+  imports: [TypeOrmModule.forFeature([Producto, _user])],
   controllers: [ProductoController],
   providers: [ProductoService],
   exports: [ProductoService], // por si lo uso en otros módulos (Carrito, Pedido, etc.)
